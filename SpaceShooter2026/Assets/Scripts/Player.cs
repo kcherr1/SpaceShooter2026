95ac2fdc68737a47a1f96b9bddf1d3a93ddaa1f0
blacklizardint <jberg@latech.edu>
2026-03-12 20:11:48 -0500

    projectile and movement script updates

--- SpaceShooter2026/Assets/Scripts/Player.cs
+++ SpaceShooter2026/Assets/Scripts/Player.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-  private SpaceShooterInputActions inputActions;
+  // set in inspector
   public float speed = 0.1f;
+  public GameObject bulletPrefab;
+  public Transform bulletSpawnPoint;
+
+  private SpaceShooterInputActions inputActions;
+  
+
+
   private const float Y_LIMIT = 4.6f;
 
   private void Start() {
@@ -12,11 +19,16 @@ public class Player : MonoBehaviour {
   }
 
   private void Update() {
+    if (inputActions.Standard.Fire.WasPressedThisFrame()) {
+      GameObject bulletObj = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+    }
+
+
     if (inputActions.Standard.MoveUp.IsPressed()) {
-      this.transform.Translate(Vector3.up * speed);
+      this.transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
     else if (inputActions.Standard.MoveDown.IsPressed()) {
-      this.transform.Translate(Vector3.down * speed);
+      this.transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
     if (this.transform.position.y > Y_LIMIT) {
       this.transform.position = new Vector3(transform.position.x, Y_LIMIT);
